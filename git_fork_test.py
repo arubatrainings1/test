99640c28a748f40b9950e1abc9387017603c213d
Joe Neville <joe.neville@hpe.com>
2021-07-20 23:25:52 +0100

    fixed by Joe

--- git_fork_test.py
+++ git_fork_test.py
@@ -1,8 +1,8 @@
 #fix the below python code, then do a pill request. 
 
 greeting ="Hello World"
-Print(greeting)
-
+print(greeting)
+#fixed by Joe
 
 #This is used to test pull request after git fork.
 #hint: there is no function name "Print()".
