94c0369fff9009c32e7617cfab67eea0c9c66307
Aruba_network_automation_training <86271427+arubatrainings1@users.noreply.github.com>
2021-07-31 12:53:38 -0700

    Re-org directories

---
+++ Lab1/git_fork_test.py
@@ -0,0 +1,9 @@
+#fix the below python code, then do a pill request. 
+
+greeting ="Hello World"
+Print(greeting)
+
+
+#This is used to test pull request after git fork.
+#hint: there is no function name "Print()".
+#######################################################################
--- git_fork_test.py
+++
@@ -1,9 +0,0 @@
-#fix the below python code, then do a pill request. 
-
-greeting ="Hello World"
-Print(greeting)
-
-
-#This is used to test pull request after git fork.
-#hint: there is no function name "Print()".
-#######################################################################
