9dbd02d8720a5fe8acc0a731ef0b0c003aa2c3e5
Aruba_network_automation_training <86271427+arubatrainings1@users.noreply.github.com>
2021-08-02 15:35:38 -0700

    Update git_fork_test.py

--- Lab1/git_fork_test.py
+++ Lab1/git_fork_test.py
@@ -5,5 +5,5 @@ Print(greeting)
 
 
 #This is used to test pull request after git fork.
-#hint: there is no function name "Print()".
+Note: Python is a case-sensitive programming language. print() is a built-in python function.The correct format is lower case, print().
 #######################################################################
