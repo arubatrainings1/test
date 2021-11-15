6a95ebfb0fbfe650027cc78c7b60b9d1f15131e1
root <cbaldwin1@gmail.com>
2021-11-15 19:52:18 +0000

    fixed the code by Chris Baldwin

--- Lab1/git_fork_test.py
+++ Lab1/git_fork_test.py
@@ -1,7 +1,8 @@
 #fix the below python code, then do a pill request. 
 
 greeting ="Hello World"
-Print(greeting)
+print(greeting)
+# this is fixed by Chris Baldwin
 
 
 #This is used to test pull request after git fork.
