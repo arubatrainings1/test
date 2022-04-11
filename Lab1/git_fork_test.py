eb8f35020639e01310eb2034c610ce6dd95b02ae
optrootmg <m.g.optroot@outlook.com>
2022-04-11 09:41:28 +0000

    fixed the code by optrootmg

--- Lab1/git_fork_test.py
+++ Lab1/git_fork_test.py
@@ -2,7 +2,8 @@
 
 greeting ="Hello World"
 print(greeting)
-#this is fixed by optrootmg
+#this is fixed by optrootmg 
+
 
 #This is used to test pull request after git fork.
 Note: Python is a case-sensitive programming language. print() is a built-in python function.The correct format is lower case, print().
