648916be5ef9b7b28055ef4a4d5fe74915ef41bf
krzychpl <krzysztof.wojciechowski@gmail.com>
2023-11-27 10:36:47 +0000

    fixed the code by me

--- Lab1/git_fork_test.py
+++ Lab1/git_fork_test.py
@@ -1,8 +1,9 @@
 #fix the below python code, then do a pill request. 
 
 greeting ="Hello World"
-Print(greeting)
+print(greeting)
 
+#this is fixed by me
 
 #This is used to test pull request after git fork.
 Note: Python is a case-sensitive programming language. print() is a built-in python function.The correct format is lower case, print().
